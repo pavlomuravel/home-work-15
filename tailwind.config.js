1559176964ebd39ed0504b7f9614768d837e8262
Pavlo Muravel <luis.proskuriv@gmail.com>
2025-01-04 05:06:05 +0100

    start

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primery: "#F8F200",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
